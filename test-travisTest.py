b43278beae94f7f5b695969e3ceb0fe01a629fb9
Eric Boxer <eboxhead@gmail.com>
2017-10-15 12:21:41 -0400

    returned testing

--- test-travisTest.py
+++ test-travisTest.py
@@ -4,7 +4,7 @@ import travisTest
 class TestTravisTest(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(travisTest.add(4,5),8)
+        self.assertEqual(travisTest.add(4,5),9)
 
 if __name__ == '__main__':
     unittest.main()
